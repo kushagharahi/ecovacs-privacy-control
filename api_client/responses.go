925aa4fa9d6dd6ded40b1d241e7d02b9e426424a
Kusha Gharahi <3326002+kushagharahi@users.noreply.github.com>
2022-03-24 03:22:01 -0500

    progress on lzma map decompressing

--- api_client/responses.go
+++ api_client/responses.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/binary"
 	"fmt"
 	"strconv"
 	"strings"
 
 	mxj "github.com/clbanning/mxj/v2"
+	"github.com/ulikunitz/xz"
 )
 
 func handleResponse(cmdName string, msgValues mxj.Map) {
-	switch cmdName {
-	case "GetMapM":
-		fmt.Printf("%s %s\n", cmdName, msgValues)
-		getMapDataValues(&msgValues)
+	fmt.Printf("%s %s\n", cmdName, msgValues)
+
+	values, err := msgValues.ValueForKey("ctl")
+
+	if err != nil {
+		err = fmt.Errorf("got error getting ctl values %w", err)
+		fmt.Println(err)
+		//todo bubble up errors
+	}
+	if values != nil {
+		valuesMap := values.(map[string]interface{})
+
+		switch cmdName {
+		case "GetMapM":
+			getMapDataValues(valuesMap)
+		case "MapP":
+			updateMapBuffer(valuesMap)
+		case "PullMP":
+			updateMapBuffer(valuesMap)
+		}
 	}
 }
 
@@ -35,12 +55,9 @@ type MapInfo struct {
 	buffer         [][]byte
 }
 
-func getMapDataValues(msgValues *mxj.Map) (*MapInfo, error) {
-	values, err := msgValues.ValueForKey("ctl")
-	if err != nil {
-		return nil, fmt.Errorf("got error getting ctl values %w", err)
-	}
-	valuesMap := values.(map[string]interface{})
+//valuesMap is not a pointer because map values are passed by reference
+func getMapDataValues(valuesMap map[string]interface{}) (*MapInfo, error) {
+
 	var mapInfo MapInfo
 
 	mapInfo.mapId, _ = strconv.Atoi(valuesMap["-i"].(string))
@@ -66,6 +83,71 @@ func getMapDataValues(msgValues *mxj.Map) (*MapInfo, error) {
 	}
 
 	return &mapInfo, nil
+}
+
+func updateMapBuffer(valuesMap map[string]interface{}) {
+	if base64EncodedString, ok := valuesMap["-p"].(string); ok {
+		fmt.Println(base64EncodedString)
+		//lzma is 7z compression
+		lmzaCompressedByteArr, err := base64.StdEncoding.DecodeString(base64EncodedString)
+		if err != nil {
+			//TODO bubble up errors
+			err = fmt.Errorf("Could not decode map piece from base64 %w", err)
+			fmt.Println(err)
+			return
+		}
+
+		lzmaCompressedByteBuffer := bytes.NewBuffer(lmzaCompressedByteArr)
+
+		//read first 5 bytes of lmzaCompressedByteArr
+		//this will equal [93 0 0 4]
+		//TODO Check these? App checks them.
+		lzmaDecoderProperties := make([]byte, 5)
+		lzmaCompressedByteBuffer.Read(lzmaDecoderProperties)
+		fmt.Println(lzmaDecoderProperties)
+
+		//read next four bytes; should represent an integer value to define the amount of bits used to represent the map data. It should be the same value as the amount determined by MapInfo.rowGrid * MapInfo.columnGrid
+		//TODO: Check assumption
+		numDataBitsByteArr := make([]byte, 4)
+		lzmaCompressedByteBuffer.Read(numDataBitsByteArr)
+		fmt.Println(numDataBitsByteArr)
+		numDataBitsInt := int(binary.LittleEndian.Uint32(numDataBitsByteArr))
+		fmt.Println(numDataBitsInt)
+
+		if err != nil {
+			//TODO Bubble up errors
+			err = fmt.Errorf("Error decoding number of data bits from []byte to int %w", err)
+			fmt.Println(err)
+			return
+		}
+
+		lzmaMapPieceByteArr := make([]byte, lzmaCompressedByteBuffer.Len())
+		lzmaCompressedByteBuffer.Read(lzmaMapPieceByteArr)
+
+		decodedMapPieceByteArr := make([]byte, numDataBitsInt)
+		lzmaReaderBuffer, err := xz.NewReader(lzmaCompressedByteBuffer)
+		if numDataBitsInt != 10000 {
+			lzmaReaderBuffer.Read(decodedMapPieceByteArr)
+		}
+
+		if err != nil {
+			//TODO Bubble up errors
+			err = fmt.Errorf("Error decoding lzma encoded map piece %w", err)
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println()
+		fmt.Println(decodedMapPieceByteArr)
+
+	}
+
+}
+
+func read_int32(data []byte) (ret int) {
+	buf := bytes.NewBuffer(data)
+	binary.Read(buf, binary.BigEndian, &ret)
+	return
 }
 
 func sliceInt64(sa []string) ([]int64, error) {
